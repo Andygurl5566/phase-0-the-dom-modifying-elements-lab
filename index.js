eb854466546d2cb26fee2658ab053c4ec83a6489
Andrea Freeman <afreeman5566@gmail.com>
2021-08-31 17:35:40 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+
 // Write your code here!
+
+ //test1 
+ main.remove()
+  
+  //test 2
+   const newHeader = document.createElement('h1') 
+
+ //test 3 
+   document.body.appendChild(newHeader) 
+
+   newHeader.id = "victory"
+
+//test 4
+
+   victory.innerHTML = "<h1> Andy is the champion</h1>"
+   
+   
+   //const id = document.createElement("victory") 
+   //newHeader.append("victory")
+   //newHeader.setAttribute('id', 'victory')
